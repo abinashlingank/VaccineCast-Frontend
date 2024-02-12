eb6f2d2cf4084f3c4d522235a9ccbca21487bdb1
abinashlingank <abinashlingank@gmail.com>
2024-02-12 10:49:32 +0530

    add axios url and warning header

--- src/App.jsx
+++ src/App.jsx
@@ -12,9 +12,14 @@ import Adminmain from './Pages/Adminmain'
 import GoogleMap from './Components/Map'
 import Vaccinecentres from './Pages/Vaccinecentres';
 
-axios.defaults.baseURL = "http://localhost:5000/";
+// axios.defaults.baseURL = "http://localhost:5000/";
+// axios.defaults.headers = {
+//   "Content-Type": "application/json",
+// };    
+axios.defaults.baseURL = "https://8f0d-182-19-35-177.ngrok-free.app";
 axios.defaults.headers = {
   "Content-Type": "application/json",
+  "ngrok-skip-browser-warning": "69420"
 };      
 
 
